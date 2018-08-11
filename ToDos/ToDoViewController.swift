34fc829dc12535bed7f4b7f8e6d199f4361c933e
Mohamed Abdulsalam <mohamedabdulsalam@mohameds-macbook-pro.local>
2018-08-11 13:11:54 -0600

    add new item function complete

--- ToDos/ToDoViewController.swift
+++ ToDos/ToDoViewController.swift
@@ -9,20 +9,42 @@
 import UIKit
 
 class ToDoViewController: UITableViewController {
-
-   let array = ["mo","malik","tariq"]
+   
+    var array = ["mo","malik","tariq", "hello"]
     @IBAction func addItembutton(_ sender: UIBarButtonItem) {
+        var alertText = UITextField()
+       
+        let alert = UIAlertController(title: "Add To Do Item", message: "", preferredStyle: .alert)
+
         
-//        let alert = UIAlertController(title: "Add To Do", message: "", preferredStyle: .alert
-//
-//        let alertAction = alertAction(
+       
+        let alertAction = UIAlertAction(title: "Add new item", style: .default) {
+            (action) in
+            
+            self.array.append(alertText.text!)
+            self.tableView.reloadData()
+            print("successful action")
+            print(alertText)
+            print(self.array)
+            
+        }
+        
+        alert.addTextField { (textAlert) in
+            textAlert.placeholder = "create new item"
+            alertText = textAlert
+        }
+        
+        
+        alert.addAction(alertAction)
+        present(alert, animated: true, completion: nil)
+        
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
-        tableView.reloadData()
+       // tableView.reloadData()
+       
     }
 
     
@@ -30,10 +52,8 @@ class ToDoViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         
-        
-        
         cell.textLabel?.text = array[indexPath.row]
-       // tableView.inputAccessoryView.ch
+       
         
         return cell
         
@@ -56,7 +76,7 @@ class ToDoViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return array.count
     }
 
 
